9421fa970820b256c548513c05d3a3472a527c55
Writaja <74670011+Writaja@users.noreply.github.com>
2022-01-21 08:46:15 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,8 @@
+noseX=0;
+noseY=0;
+leftWrist=0;
+rightWrist=0;
+difference=0;
 function preload(){
 
 }
@@ -11,6 +16,10 @@ posenet.on("pose",gotposes);
 }
 function draw(){
 background("#8f8d88");
+document.getElementById("squaresize").innerHTML="width and height of the square will be ="+difference+"px";
+fill("#fc03b1");
+stroke("#fc03b1");
+square(noseX,noseY,difference);
 }
 function modelloaded(){
     console.log("modelloaded");
@@ -18,5 +27,14 @@ function modelloaded(){
 function gotposes(results){
     if (results.length>0){
         console.log(results);
+        noseX=results[0].pose.nose.x;
+noseY=results[0].pose.nose.y;
+console.log(noseX,noseY);
+leftWrist=results[0].pose.leftWrist.x;
+rightWrist=results[0].pose.rightWrist.x;
+difference=floor(leftWrist-rightWrist);
+console.log(leftWrist,rightWrist,difference);
+
+
     }
 }
